79e1c2eac36bf14beae446fb618d0eec5488af97
burakkiraz <burak.kiraz@boun.edu.tr>
2021-03-21 23:56:53 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+import requests
+
+api_key = ""
+#in order to get a free api_key go to https://fixer.io/signup/free and sign up for free plan. Then, you can get an api_key for free
+
+url = "http://data.fixer.io/api/latest?access_key="+api_key
+
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET","POST"])
+def index():
+    if request.method == "POST":
+        firstCurrency = request.form.get("firstCurrency") #USD
+        secondCurrency = request.form.get("secondCurrency") #TRY
+
+        amount = request.form.get("amount") #15
+        response = requests.get(url)
+        infos = response.json()
+
+        firstValue = infos["rates"][firstCurrency]
+        secondValue = infos["rates"][secondCurrency]
+
+        result = (secondValue / firstValue) * float(amount)
+
+        cureencyInfo = dict()
+        cureencyInfo["firstCurrency"] = firstCurrency
+        cureencyInfo["secondCurrency"] = secondCurrency
+        cureencyInfo["amount"] = amount
+        cureencyInfo["result"] = result
+
+        return render_template("index.html",info = cureencyInfo)
+
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
